03277dace9782fd75a226b2e4ed5286dac9568d1
lixiping <lixp@sctek.com>
2019-09-27 10:25:47 +0800

    index

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import TopTable from './TopTable.vue'
+import PanelComponent from './PanelComponent.vue'
+import PhotoWall from './PhotoWall.vue'
+import MySwitch from './MySwitch.vue'
+export default{
+  TopTable,
+  PanelComponent,
+  PhotoWall,
+  MySwitch,
+}
